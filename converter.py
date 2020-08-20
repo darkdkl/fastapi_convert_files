589905ce1c28c3219d80d1d2509ae7b3a79a74a3
Dark_Dmake <dark.dmake@gmail.com>
2020-08-20 12:50:41 +0300

    add response file

--- converter.py
+++ converter.py
@@ -3,8 +3,6 @@ import subprocess
 def convert_doc_to_pdf(file_path,filename):
     args = ["libreoffice", '--headless', '--convert-to', 'pdf', '--outdir', ".",
             f"{file_path}"]
-    converter = subprocess.run(args,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE
-                               )
+    converter = subprocess.run(args,stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
     return converter.returncode
--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import os
 import tempfile
 import shutil
-from fastapi import FastAPI, File
+import time
+
+from fastapi import FastAPI, File,BackgroundTasks
 from validators import UploadDocImgFile
 from converter import convert_doc_to_pdf
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+
 
 @app.post('/')
 async def load_file(file: UploadDocImgFile = File(...)):
     with tempfile.NamedTemporaryFile() as temp:
         shutil.copyfileobj(file.file,temp)
         convert_doc_to_pdf(temp.name,file.filename)
-        os.renames(f"{temp.name.split('/')[-1]}.pdf",
-                   f'{file.filename.split(".")[0]}.pdf')
-    return {"ok": ''}
+        new_file= f'{file.filename.split(".")[0]}.pdf'
+        os.renames(f"{temp.name.split('/')[-1]}.pdf",new_file )
+    return {'file': new_file}
 
+
+def remove_file(file:str):
+    print('до')
+    time.sleep(2)
+    os.remove(os.path.abspath(file))
+    print("после")
+
+
+@app.get('/file/')
+async def get_file(file: str,background_tasks: BackgroundTasks):
+    if os.path.exists(file):
+        background_tasks.add_task(remove_file,file)
+        return FileResponse(os.path.abspath(file))
+
+#именование файлов
